616bce08ac8fb88839b87f45d280e3c38af9fccb
Fan Yang <stoneyang0915@gmail.com>
2014-04-02 18:24:53 +0800

    ENH: fixed the no _set_params attribute error

--- scikits/crab/recommenders/svd/classes.py
+++ scikits/crab/recommenders/svd/classes.py
@@ -286,7 +286,7 @@ class MatrixFactorBasedRecommender(SVDRecommender):
                  Desired number of recommendations (default=None ALL)
 
         '''
-        self._set_params(**params)
+        # self._set_params(**params)
 
         candidate_items = self.all_other_items(user_id)
 
